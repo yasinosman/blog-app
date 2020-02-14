384043b7f241f16239a2f7f9b213629d70f189aa
Yasin <yasinosman10@gmail.com>
2020-02-14 18:09:52 +0300

    fixed comment and listener

--- app.js
+++ app.js
@@ -49,12 +49,9 @@ app.use(methodOverride("_method"));
 //seedDB();
 
 //Mongodb connection
-mongoose.connect("mongodb://localhost/cydd-kutuphane-app", 
-    {   
-        useNewUrlParser: true,
-        useUnifiedTopology: true 
-    }
-);
+mongoose.connect( "mongodb+srv://yasinosman:123**saatkac123@blog-app-wx0nf.mongodb.net/test?retryWrites=true&w=majority",
+{ useNewUrlParser: true, useUnifiedTopology: true }, 
+() => { console.log("we are connected")}).catch(err => console.log(err));
 
 //Routes
 app.use(commentRoutes);
@@ -62,6 +59,6 @@ app.use(indexRoutes);
 app.use(blogRoutes);
 
 //Express listener
-app.listen(port, process.env.IP, function(){
+app.listen(process.env.PORT || 5000, process.env.IP, function(){
     console.log(`Example app listening on port ${port}!`);
 });
